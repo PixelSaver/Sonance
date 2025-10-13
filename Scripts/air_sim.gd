d2ae9d461a7ef2375b7dda3762bf0af8253015b3
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-12 22:49:46 -0700

    pipeline!!

--- Scripts/air_sim.gd
+++ Scripts/air_sim.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const GRID_SIZE := Vector2(32, 16)  # Small grid to start
+const GRID_SIZE := Vector2i(32, 16)  # Small grid to start
 const CELL_SIZE = 16  # Pixels per cell (for drawing)
 const SHADER_FILE_PATH = "res://ignore/acoustic_wave.glsl"
 
@@ -23,7 +23,24 @@ func _ready() -> void:
 	var shader_spirv: RDShaderSPIRV = shader_file.get_spirv()
 	shader = rd.shader_create_from_spirv(shader_spirv)
 	
-	# Actually make input stuff
+	# Actually make input buffer and stuff
+	grid_data = PackedFloat32Array()
+	grid_data.resize(GRID_SIZE.x * GRID_SIZE.y)
+	grid_data.fill(0.0)
 	
+	# Passing in information to the shader
+	# Called uniforms in glsl i think
+	var uniform = RDUniform.new()
+	uniform.uniform_type = RenderingDevice.UNIFORM_TYPE_STORAGE_BUFFER
+	uniform.binding = 0 # needs to match the "binding" in shader file (from docs)
+	uniform.add_id(buffer)
+	uniform_set = rd.uniform_set_create([uniform], shader, 0)
 	
+	# Create a compute pipeline (from docs)
+	pipeline = rd.compute_pipeline_create(shader)
+	var compute_list := rd.compute_list_begin()
+	rd.compute_list_bind_compute_pipeline(compute_list, pipeline)
+	rd.compute_list_bind_uniform_set(compute_list, uniform_set, 0)
+	rd.compute_list_dispatch(compute_list, 5, 1, 1)
+	rd.compute_list_end()
 	
