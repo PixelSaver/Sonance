1a57e43a9f711cf1f4a51225c0aec20fb3d4f73c
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-15 09:55:58 -0700

    got knob turning to work!

--- Scripts/radio.gd
+++ Scripts/radio.gd
@@ -3,11 +3,12 @@ class_name Radio
 
 @export var knob1 : RigidBody3D
 @export var knob2 : RigidBody3D
-@export var sensitivity : float = 1.
+@export var sensitivity : float = 0.1
 var tuning : float = 0.
 var volume : float = 1.
 var knob_hovered : RigidBody3D
 var clicked = false
+var dragging = false
 
 func _ready():
 	knob1.connect("mouse_entered", _on_knob_1)
@@ -16,11 +17,14 @@ func _ready():
 	knob2.connect("mouse_exited", _on_knob_exit)
 	
 func _on_knob_1():
+	if dragging: return
 	knob_hovered = knob1
 func _on_knob_2():
+	if dragging: return
 	knob_hovered = knob2
 func _on_knob_exit():
-	knob_hovered = null
+	#knob_hovered = null
+	pass
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -30,23 +34,27 @@ func _input(event: InputEvent) -> void:
 				print("Mouse down")
 			else:
 				print("Mouse up")
+				dragging = false
 
-	elif event is InputEventMouseMotion:
+	if event is InputEventMouseMotion:
 		if clicked and knob_hovered:
-			tuning += -event.relative.y * sensitivity
-			tuning = clamp(tuning, 0.0, 100.0) 
-			update_knob_turn()
+			dragging = true
+		update_knob_turn(event as InputEventMouseMotion)
 	
 
 
-func update_knob_turn():
+func update_knob_turn(event:InputEventMouseMotion):
+	if not dragging: return
 	match knob_hovered:
 		null:
 			return
 		knob1:
+			tuning += (-event.relative.y + event.relative.x) * sensitivity
 			print("Knob value:", tuning)
 			knob1.rotation.z = tuning
 		knob2:
-			pass
+			volume += (-event.relative.y + event.relative.x) * sensitivity
+			print("Knob value:", volume)
+			knob1.rotation.z = volume
 	
 	
