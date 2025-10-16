bdbd9c09e47611264d86026a52e8ef4a3c9371d5
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-16 14:25:38 -0700

    Added animation to turn camera around

--- Scripts/UI.gd
+++ Scripts/UI.gd
@@ -9,12 +9,14 @@ func _ready() -> void:
 	Global.game_state_changed.connect(_on_state_change)
 
 func _on_button_mouse_entered() -> void:
+	if Global.state != Global.States.MENU: return
 	if t: t.kill()
 	t = create_tween().set_ease(Tween.EASE_OUT)
 	t.set_parallel(true).set_trans(Tween.TRANS_QUINT)
 	t.tween_property(button, "scale", Vector2.ONE * 1.1, 0.3)
 	t.tween_property(button, "modulate", Color.SANDY_BROWN, 0.3)
 func _on_button_mouse_exited() -> void:
+	if Global.state != Global.States.MENU: return
 	if t: t.kill()
 	t = create_tween().set_ease(Tween.EASE_OUT)
 	t.set_parallel(true).set_trans(Tween.TRANS_QUINT)
