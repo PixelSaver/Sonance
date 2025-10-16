5540fc396c4f6e4be657c0602e5f2e60aabfa561
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-10-15 22:34:20 -0700

    trying unprojection

--- Scripts/camera_3d.gd
+++ Scripts/camera_3d.gd
@@ -1,11 +1,15 @@
 extends Camera3D
 
 @export var ray : RayCast3D
+@export var canvas_target : Marker3D
+@export var control : Control
 
 func _process(_delta: float) -> void:
 	var mouse_pos = get_viewport().get_mouse_position()
-	#mouse_pos = mouse_pos /2 - Vector2.ONE / 2 
 	var from = project_ray_origin(mouse_pos)
 	var to = from + project_ray_normal(mouse_pos) * 100
 	ray.global_position = from
 	ray.target_position = to
+	
+	var screen_pos = unproject_position(canvas_target.global_position)
+	control.position = screen_pos
